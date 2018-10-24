bfcbe230bd271dbca26f2864cad4c37126e41523
Ahmed Jerid <ahmd.jerid@gmail.com>
2018-10-24 15:37:27 +0200

    use services

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -22,12 +22,18 @@ export class HeroesComponent implements OnInit {
       
     }
 
-  heroes = HEROES;
+    heroes: Hero[];
 
 
-  constructor() { }
-
-  ngOnInit() {    
+  constructor(private heroService: HeroService) {     
   }
 
+  ngOnInit() { 
+    this.getHeroes();
+   
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
